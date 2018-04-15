6d408aa54be7a0db443291d2245f91abb3bbce85
thejerrytan <jerrytansk@gmail.com>
2018-04-15 22:27:55 +0800

    report images, redo track1 video

--- train.py
+++ train.py
@@ -6,7 +6,19 @@ import sklearn
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
+# Use this to train track2
 TRAINING_DIR = "harder_data"
+# Use this to train track1
+# TRAINING_DIR = "data"
+
+# Model filename for track1
+# MODEL_FILENAME = "model.h5"
+# Model filename for track2
+MODEL_FILENAME = "model_hard.h5"
+
+# Steering angle correction factor for left and right cameras
+CORRECTION_FACTOR = 0.20
+
 samples = []
 def load_data():
     with open('./{TRAINING_DIR!s}/driving_log.csv'.format(**globals())) as csvfile:
@@ -39,10 +51,10 @@ def random_shift(img, angle, shift_range):
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
-    new_batch_size = batch_size // 9
+    new_batch_size = batch_size // 9 # Because each loop generates x9 more images
     while 1: # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
-        for offset in range(0, num_samples, new_batch_size): # Because each loop generates x6 more images
+        for offset in range(0, num_samples, new_batch_size): 
             batch_samples = samples[offset:offset+new_batch_size]
 
             images = []
@@ -63,7 +75,9 @@ def generator(samples, batch_size=32):
             yield sklearn.utils.shuffle(X_train, y_train)
 
 # Data augmentation
+saved = False
 def preprocess_data(source_path, measurement, images, angles):
+    global saved
     image = cv2.imread('./{TRAINING_DIR!s}/IMG/'.format(**globals()) + source_path.split(os.sep)[-1])
     b,g,r = cv2.split(image)       # get b,g,r
     image = cv2.merge([r,g,b])     # switch it to rgb
@@ -82,9 +96,16 @@ def preprocess_data(source_path, measurement, images, angles):
     translated, new_steering_angle = random_shift(image, measurement, 10)
     images.append(translated)
     angles.append(new_steering_angle)
-    
+    if not saved:
+        from scipy.misc import imsave # imsave saves images in R,G,B order
+        # imsave("./report/track1_normal.jpg", image)
+        # imsave("./report/track1_flipped.jpg", flipped)
+        # imsave("./report/track1_translated.jpg", translated)
+        imsave("./report/track2_normal.jpg", image)
+        imsave("./report/track2_flipped.jpg", flipped)
+        imsave("./report/track2_translated.jpg", translated)
+        saved = True
 
-CORRECTION_FACTOR = 0.20
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Dropout
@@ -104,9 +125,9 @@ def main():
     validation_generator = generator(validation_samples, batch_size=32)
 
     # Compile and train model
-    # if os.path.isfile("model_hard.h5"):
+    # if os.path.isfile(MODEL_FILENAME):
     if False:
-        model = load_model('model_hard.h5')
+        model = load_model(MODEL_FILENAME)
     else:
         model = Sequential()
         model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(80,160,3)))
@@ -126,7 +147,7 @@ def main():
     model.fit_generator(train_generator, nb_epoch=20, samples_per_epoch=len(train_samples), validation_data=validation_generator, \
                         nb_val_samples=len(validation_samples))
 
-    model.save('model_hard.h5')
+    model.save(MODEL_FILENAME)
 
 if __name__ == "__main__":
     main()
